0918b2ce695c6dd22b4c565a3edf611766f65c0c
Osama <osamazaheer057@gmail.com>
2022-10-01 19:05:59 +0530

    implement functionality to pick image from gallery and select image category

--- app/src/main/java/com/example/collegemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/collegemanagementapp/MainActivity.java
@@ -10,21 +10,34 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    CardView uploadNotice;
+   private CardView uploadNotice;
+  private   CardView addGalleryImage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         uploadNotice = findViewById(R.id.addNotice);
+        addGalleryImage =findViewById(R.id.addGalleryImage);
+
         uploadNotice.setOnClickListener(this);
+        addGalleryImage.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()){
+
             case R.id.addNotice:
-                Intent intent = new Intent(MainActivity.this,UploadNotice.class);
+                 intent = new Intent(MainActivity.this,UploadNotice.class);
+                startActivity(intent);
+                break;
+            case R.id.addGalleryImage:
+                 intent = new Intent(MainActivity.this,UploadImage.class);
                 startActivity(intent);
                 break;
         }
